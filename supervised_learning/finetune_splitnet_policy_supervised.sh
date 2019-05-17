857db730d43cacffdd582c2626e04732c25fee74
Abhishek Kadian <abhishekkadiyan@gmail.com>
2019-05-17 14:09:09 -0700

    Add header to shell scripts

--- supervised_learning/finetune_splitnet_policy_supervised.sh
+++ supervised_learning/finetune_splitnet_policy_supervised.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 DATASET="mp3d"
 
 export GLOG_minloglevel=2
--- supervised_learning/train_imagenet.sh
+++ supervised_learning/train_imagenet.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 python supervised_learning/imagenet_pretrain.py \
     --log-prefix output_files/imagenet/pretrain \
     --workers 40 \
