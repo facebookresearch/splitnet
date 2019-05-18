74de36a27dce6489c7cdb2471a9734aff7d4e0d2
Daniel <danielgordon10@gmail.com>
2019-05-17 18:02:26 -0700

    mit -> cc

--- download_weights.sh
+++ download_weights.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) Facebook, Inc. and its affiliates.
-# This source code is licensed under the MIT license found in the
+# This source code is licensed under the Creative Commons license found in the
 # LICENSE file in the root directory of this source tree.
 
 WEIGHT_DIRECTORY="output_files"
